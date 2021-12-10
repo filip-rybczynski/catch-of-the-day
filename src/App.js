4b5386698d9766e8f3f4b8d27adb757010a938fd
Filip Rybczyński <filipryba@gmail.com>
2021-12-11 00:20:36 +0100

    Adding fish to the App state using the Inventory form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import React from "react";
-import Header from './components/Header';
-import Order from './components/Order';
-import Inventory from './components/Inventory';
+import Header from "./components/Header";
+import Order from "./components/Order";
+import Inventory from "./components/Inventory";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    fishMenu: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    const fishMenu = { ...this.state.fishMenu };
+
+    fishMenu[`fish${Date.now()}`] = fish;
+
+    this.setState({
+      fishMenu, // can be used instead of fishMenu: fishMenu
+    });
+  };
+
+  // TODO After course is finished, re-write the code without refs
+  //   addFish = (e) => {
+  //     e.preventDefault();
+
+  //     const formValues = [...e.target.elements].slice(0, -1);
+  //     const newFish = {};
+
+  //     formValues.forEach(element => {
+  //       const {value, name, type} = element;
+  //       if(type === "number") {
+  //         newFish[name] = parseFloat(value);
+  //       } else if (type === "select-one") {
+  //         newFish.available = (value === "available" ? true : false);
+  //     } else {
+  //       newFish[name] = value;
+  //     }
+
+  //     const fishMenu = {...this.state.fishMenu};
+
+  //     fishMenu[`fish${Date.now()}`] = newFish;
+
+  //     this.setState({
+  //       fishMenu: fishMenu,
+  //     })
+  //   }
+  //     );
+  // }
 
   render() {
+    const { fishMenu } = this.state;
     return (
-        <div className="catch-of-the-day">
-            <div className="menu">
-                <Header tagline="Fresh Seafood Market"/>
-            </div>
-            <Order />
-            <Inventory />
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
         </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
     );
   }
 }
--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -6,14 +6,41 @@ class AddFishForm extends React.Component {
     this.state = {};
   }
 
+// TODO remove refs and use controlled components
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (e) => {
+    e.preventDefault();
+
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value === "available" ? true : false,
+    };
+
+    this.props.addFish(fish);
+
+    e.target.reset();
+  };
+
   render() {
     return (
-      <form>
-        <input name="name" type="text" placeholder="Name"></input>
-        <input name="price" type="text" placeholder="Price"></input>
-        <input name="status" type="text" placeholder="Status"></input>
-        <input name="desc" type="text" placeholder="Desc"></input>
-        <input name="image" type="text" placeholder="Image"></input>
+      <form className="fish-edit" onSubmit={this.props.addFish}>
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="number" placeholder="Price" />
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" placeholder="Description" />
+        <input name="image" type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish </button>
       </form>
     );
   }
