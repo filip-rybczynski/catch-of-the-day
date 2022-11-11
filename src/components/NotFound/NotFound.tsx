ca869b6e6fa683f8918982b132b73506b862df87
Filip Rybczyński <filipryba@gmail.com>
2022-11-11 22:55:21 +0100

    NotFound component: added button to go to StorePicker
    Button uses RouteComponentProps' history prop to jump to StorePicker

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,20 +1,29 @@
+// React
+import { RouteComponentProps } from "react-router";
+
 // assets
 import deadFish from "./assets/dead-fish.png";
 
 // styles
 import "./NotFound.styles.scss";
 
-
-export const NotFound = () => {
-
+export const NotFound = ({ history }: RouteComponentProps) => {
   return (
     <div className="not-found">
-      <h2 className="not-found__message">Not Found!!</h2>
+      <h2 className="not-found__message">Not Found!</h2>
       <img
         src={deadFish}
         alt="dead cartoon fish"
         className="not-found__image"
       />
+      <button
+        className="not-found__button"
+        onClick={() => {
+          history.push("/"); // Go to Store Picker
+        }}
+      >
+        Go to store selection
+      </button>
     </div>
   );
 };
